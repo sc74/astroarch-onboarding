c43e7e33c843461fc73c2d8b5227b56f79e9475c
sc74 <scarlin74@gmail.com>
2025-03-26 19:35:05 +0000

    update script

--- src/.astroarch/configs/calamares_astroarch/run-astroarch-onboarding.sh
+++ src/.astroarch/configs/calamares_astroarch/run-astroarch-onboarding.sh
@@ -9,9 +9,6 @@ rm -f /root/.zshrc
 ln -s /home/astronaut/.astroarch/configs/.zshrc /root/.zshrc
 chsh -s /usr/bin/zsh root
 
-# link to folder autostart
-ln -s /usr/share/calamares/.astroarch/configs/calamares_astroarch/AstroArch-onboarding.desktop /home/astronaut/.config/autostart/AstroArch-onboarding.desktop
-
 # Allow root access with no password to astroarch-onboarding.desktop
 rm -f /usr/share/polkit-1/actions/com.github.calamares.calamares.policy
 ln -s /usr/share/calamares/.astroarch/configs/calamares_astroarch/com.github.calamares.calamares.policy /usr/share/polkit-1/actions/com.github.calamares.calamares.policy
