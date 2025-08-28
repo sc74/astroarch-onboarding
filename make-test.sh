157b46e6b83c04d30e1dff729320319a7690e6b1
sc74 <scarlin74@gmail.com>
2025-08-28 18:13:31 +0100

    Update and cleanup

--- make-test.sh
+++ make-test.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_QT6=ON ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DWITH_QT6=ON -DINSTALL_CONFIG=ON -DSKIP_MODULES="bootloader contextualprocess displaymanager dracut dracutlukscfg dummycpp dummyprocess dummypython fsresizer fstab grubcfg initcpio initcpiocfg initramfscfg luksbootkeyfile luksopenswaphookcfg mkinitfs mount netinstall networkcfg notesqml openrcdmcryptcfg oemid partition plymouthcfg preservefiles rawfs removeuser services-openrc services-systemd tracking umount unpackfs users usersq zfs zfshostid" ..
 make -j$(nproc)
 #sudo make install
--- make.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DWITH_QT6=ON -DINSTALL_CONFIG=ON ..
-make -j$(nproc)
-sudo make install
