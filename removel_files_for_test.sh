fb4a15b19903a0946b9176bd342b5ccbcf2b29cb
sc74 <scarlin74@gmail.com>
2025-09-05 01:51:33 +0200

    Minor updates2

--- removel_files_for_test.sh
+++ removel_files_for_test.sh
@@ -7,6 +7,9 @@ sudo rm /usr/share/polkit-1/actions/com.github.calamares.calamares.policy
 rm /home/astronaut/.astroarch/configs/netinstall_software.yaml
 rm -rf /home/astronaut/.astroarch/plugins/power_max_current
 sudo rm -rf /usr/share/calamares
+sudo rm /usr/lib/calamares/libcalamares.so
+sudo rm /usr/lib/libcalamares.so
+sudo rm /usr/lib/libcalamaresui.so
 
 # Uninstall
 cd build
