469099e3ec2f2e631573bf56cb2e8d3e9f513442
sc74 <sc74@gmail.com>
2025-10-20 10:16:40 +0200

    Update for Astroarch 2.0

--- src/usr/share/astroarch_onboarding/configs/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i 's/%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
-
-su astronaut -c "zsh -c 'source /home/astronaut/.zshrc && update-astroarch'"
-
-sed -i '0,/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
